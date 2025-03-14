8848f3a80104777b94a1d5c3b1a63a0db71008d8
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2025-03-14 23:50:30 +0600

    inslagtion done

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const section = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll('nav a[href^="#"]');
+const menu = document.querySelectorAll(".navbar-menu");
+const burger = document.querySelectorAll(".navbar-burger");
+const close = document.querySelectorAll(".navbar-close");
+const backrrop = document.querySelectorAll(".navbar-backdrop");
+const mobileMenuLinks = document.querySelectorAll("#mobile-menu a[href^='#']");
+console.log(mobileMenuLinks);
